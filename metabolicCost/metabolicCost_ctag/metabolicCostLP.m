b5a2eedeee8221068171e11dfdbb47a19ca4e56f
jamatics <jawwadmasood@gmail.com>
2020-02-20 19:23:46 +0100

    Added some place holder files to start

---
+++ metabolicCost/metabolicCost_ctag/metabolicCostLP.m
@@ -0,0 +1,27 @@
+    % ----------------------------------------------------------------------- %
+    % The tedSR  API is a toolkit for scalable performance metrics and        %
+    % simulation. See http://eurobench2020.eu/ and the NOTICE file            %
+    % for more information. TedSR is developed at Fundación para la Promoción %
+    % de la Automoción en Galicia. CTAG, and supported by European Comission  %
+    % H2020 Progamme under TestEd FTP Eurobench.			%
+    %                                                                         %
+    % Copyright (c) 2019-2020 CTAG and the authors                            %
+    % Author(s): Jawad Masood & Federico Macció                               %
+    %                                                                         %
+    % Licensed under the Apache License, Version 2.0 (the "License");         %
+    % you may not use this file except in compliance with the License.        %
+    % You may obtain a copy of the License at                                 %
+    % http://www.apache.org/licenses/LICENSE-2.0.                             %
+    %                                                                         %
+    % Unless required by applicable law or agreed to in writing, software     %
+    % distributed under the License is distributed on an "AS IS" BASIS,       %
+    % WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or         %
+    % implied. See the License for the specific language governing            %
+    % permissions and limitations under the License.                          %
+    % ----------------------------------------------------------------------- %
+    %                                                                         %
+    % metaBolicCostLP.m                                                              %
+    % Author(s): Jawad Masood						      %
+    % ----------------------------------------------------------------------- %
+
+
