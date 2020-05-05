51f67fb9039a0a3da98d12e91f166e1dc8faee04
maaldo <mariana.alves@ctag.com>
2020-05-05 07:32:54 -0300

    Alg. is added that first read, sum and then classify the use experience

--- borgScale10/borgScale10_ctag/src/borgScale10LP.m
+++ borgScale10/borgScale10_ctag/src/borgScale10LP.m
@@ -3,10 +3,10 @@
     % simulation. See http://eurobench2020.eu/ and the NOTICE file            %
     % for more information. TedSR is developed at Fundación para la Promoción %
     % de la Automoción en Galicia. CTAG, and supported by European Comission  %
-    % H2020 Progamme under TestEd FTP Eurobench.			%
+    % H2020 Progamme under TestEd FTP Eurobench.			                        %
     %                                                                         %
     % Copyright (c) 2019-2020 CTAG and the authors                            %
-    % Author(s): Jawad Masood & Federico Macció                               %
+    % Author(s): Jawad Masood                                                 %
     %                                                                         %
     % Licensed under the Apache License, Version 2.0 (the "License");         %
     % you may not use this file except in compliance with the License.        %
@@ -20,8 +20,34 @@
     % permissions and limitations under the License.                          %
     % ----------------------------------------------------------------------- %
     %                                                                         %
-    % borgScale10LP.m                                                              %
+    % borgScale10LP.m                                                         %
     % Author(s): Jawad Masood						      %
     % ----------------------------------------------------------------------- %
+    
+    
+pkg load io;
+pkg load signal;
 
+clear; 
+close all;
+clc;
+
+sqr = csv2cell('/home/osboxes/repo/pi_ctag/borgScale10/borgScale10_ctag/test_data/input/questionnaire5_narrow_1.csv', ';');
+sumResult = sum(cell2mat(sqr(2:end,2)));
+
+if (sumResult == 0)
+  globalRes = ('None');
+elseif (sumResult >= 1 && sumResult <= 16)
+  globalRes = ('Annoying');
+elseif (sumResult >= 17 && sumResult <= 32)
+  globalRes = ('Uncomfortable');
+elseif (sumResult >= 33 && sumResult <=48)
+  globalRes = ('Dreadful');
+elseif (sumResult >= 49 && sumResult <= 64)
+  globalRes = ('Horrible');  
+elseif (sumResult >= 65)
+  globalRes = ('Agonizing');
+endif
+
+globalRes
 
