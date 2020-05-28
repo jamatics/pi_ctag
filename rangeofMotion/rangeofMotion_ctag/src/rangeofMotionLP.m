5df8ebc078d0f6b81449e5baa04d4809ef734556
jamatics <jawwadmasood@gmail.com>
2020-05-28 20:27:12 +0500

    Started adding code to RoM with verification if it works OK with EB format

--- rangeofMotion/rangeofMotion_ctag/src/rangeofMotionLP.m
+++ rangeofMotion/rangeofMotion_ctag/src/rangeofMotionLP.m
@@ -23,26 +23,83 @@
     % rangeofMotionLP.m                                                              %
     % Author(s): Jawad Masood						      %
     % ----------------------------------------------------------------------- %
-clear,clc, close all
+clear; 
+close all;
+clc;
 
-% We need to check the following code before
-DatosSubjects =load(sprintf (".\\tests\\data\\input\\%sa%s.txt", subject,run));
+csv_file = "../tests/data/input/subject_03_trial_01_jointAngles.csv";
+result_dir = "../tests/data/output";
 
-% EXTRAER DATOS DE ETIQUETAS, MAXIMOS Y MINIMOS DE CADA EJE Y ARTICULACION
+disp(["Input parameters: ", csv_file, " ", result_dir])
+    isOctave = exist('OCTAVE_VERSION', 'builtin') ~= 0;
 
-for i=2:length(DatosSubjects.data(1,:))  
-  Datos_Maximos_Minimos{1,i-1} = max(DatosSubjects.data(:,i));  % Datos Maximos
-  Datos_Maximos_Minimos{2,i-1} = min(DatosSubjects.data(:,i));  % Datos Minimos
+if isOctave
+        disp('Using Octave')
+        pkg load signal;
+        pkg load mapping;
+        pkg load statistics;
+        pkg load io;
+    else
+        disp('Using Matlab')
+    end
+
+
+angles = csv2cell(csv_file, ',');
+header = angles(1, :);
+angles = cell2mat(angles(2:end, :));
+l = size(angles);
+
+for i = 2:l(2)
+max_min_med_Data {1,i-1} = header(1,i);
+max_min_med_Data {2,i-1} = max(angles(2:end,i));
+max_min_med_Data {3,i-1} = min(angles(2:end,i));
+max_min_med_Data {4,i-1} = median(angles(2:end,i));
+endfor
+
+
+counterflexion=0;
+counterextension=0;
+counter = 0;
+
+for i = 1:length(header)
+  % -----------------   for r_hip_x --------------------- %
+  if strcmp(header{1,i},'r_hip_x')
+    limit_position{1,1} = 'r_hip_x';
+    fprintf('R HIP X --> Exist in the column: %d\n',i);
+    for j = 1:length(angles(:,i))
+      if angles(j,i) >= 100
+        counter == counter + 1;
+        limit_position{counter + 1, 1} = angles(j,i);
+      end
+      if angles(j,i) <= -30
+        counter = counter + 1;
+        limit_position{counter + 1,1} = angles(j,i);
+      end
+    end
+    counterflexion=0;
+    counterextension=0;
+    counter = 0;   
+  end
+ % -----------------   para r_knee_x --------------------- %
+ if strcmp(header{1,i}, 'r_knee_x')
+   limit_position{1,2} = 'r_knee_x';
+   fprintf('R KNEE X --> Exist in the column: %d\n',i);
+   for j = 1:length(angles(:,i))
+      if angles(j,i) >= 140
+        counter == counter + 1;
+        limit_position{counter + 1, 2} = angles(j,i);
+      end
+      if angles(j,i) <= -10
+        counter = counter + 1;
+        limit_position{counter + 1,2} = angles(j,i);
+      end
+    end
+    counterflexion=0;
+    counterextension=0;
+    counter = 0;   
+  end   
 end
 
 
-% We will include more code here to check the complaince of target joint angles with the relevant standard. Work in progress.
-
-% PLOTEAR
 
 
-for i=1:length(Datos_Maximos_Minimos)
-    hold on 
-    plot(i,Datos_Maximos_Minimos{1,i},'r*'); % Valores maximos de color rojo
-    plot(i,Datos_Maximos_Minimos{2,i},'g*'); % Valores minimos de color verde 
-end
